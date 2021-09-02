ba36d3013ff50adbe6b2cafe452818aae912a7a9
Fitch <fitchj@purdue.edu>
2021-09-02 17:29:49 -0400

    More updates

--- src/framework/master.py
+++ src/framework/master.py
@@ -1,7 +1,22 @@
 import numpy as np
 import user_input
 
-# pseudo-code 
 
-inputs = user_input.user_input()
+def single_run_launch_platform_6dof(weather_model):
 
+
+    # Code for launch platform 6dof goes here!
+
+    run_result = 1
+    return run_result, drift_altitude
+
+
+if "__main__":
+    inputs = user_input.user_input()
+    if inputs.mode == 1:
+        run_status, drift_altitude = single_run_launch_platform_6dof(inputs.weather_model)
+        if run_status == -1:
+            print('Run failed!')
+        elif run_status == 1:
+            print('Run succeeded!')
+            "Balloon drifted to an altitude of {} km".format()
--- src/framework/user_input.py
+++ src/framework/user_input.py
@@ -1,18 +1,35 @@
-def user_input():
-    GUI = 'initialize'
-    while GUI != 'Yes' or 'No':
-        GUI = input('Would you like to use the GUI (Yes/No): ')
-        if GUI != 'Yes' or 'No':
-            print('Incorrect input, expected /"Yes/" or /"No/"')
-    if GUI == 'Yes':
-        input = user_input_GUI()
-    elif GUI == 'No':
-        input = user_input_terminal()
-    return input
+import datetime as t
 
-def user_input_GUI():
+
+def user_input():
+    gui = 'initialize'
+    gui = raw_input('Would you like to use the GUI (Yes/No): ')
+    while gui not in ['Yes', 'No']:
+        print('\nIncorrect input, expected "Yes" or "No"\n')
+        gui = raw_input('Would you like to use the GUI (Yes/No): ')
+    if gui == 'Yes':
+        input_data = user_input_gui()
+    elif gui == 'No':
+        input_data = user_input_terminal()
+    else:
+        input_data = None
+    return input_data
+
+
+def user_input_gui():
     pass
     # TODO: develop a GUI interface for MASTRAB (lmao terrible name)
+    return inputs
+
 
 def user_input_terminal():
-    pass
+    # Fake Date to test inputs
+    inputs = InputStructure()
+    inputs.launch_date = t.datetime(2025, 06, 21, 7)
+    inputs.mode = 1
+    return inputs
+
+
+class InputStructure:
+    def __init__(self):
+        self.launch_alt = 20000
