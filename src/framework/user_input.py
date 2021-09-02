d1c424beaf71acfdc94bd36195d0bb560e78fc67
Joshua Fitch <44382285+Josh-Fitch@users.noreply.github.com>
2021-09-02 16:33:54 -0400

    First few updates

--- src/framework/user_input.py
+++ src/framework/user_input.py
@@ -1,2 +1,18 @@
 def user_input():
+    GUI = 'initialize'
+    while GUI != 'Yes' or 'No':
+        GUI = input('Would you like to use the GUI (Yes/No): ')
+        if GUI != 'Yes' or 'No':
+            print('Incorrect input, expected /"Yes/" or /"No/"')
+    if GUI == 'Yes':
+        input = user_input_GUI()
+    elif GUI == 'No':
+        input = user_input_terminal()
+    return input
+
+def user_input_GUI():
+    pass
+    # TODO: develop a GUI interface for MASTRAB (lmao terrible name)
+
+def user_input_terminal():
     pass
