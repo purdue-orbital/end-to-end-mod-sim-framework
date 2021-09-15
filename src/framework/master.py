1dd204b776db929a81066dcd276cd656d14a25e5
Matthew Popplewell <matthew.popplewell@advancedspace.com>
2021-09-15 19:22:43 -0400

    Initial work

--- src/framework/master.py
+++ src/framework/master.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 import datetime as t
 
 # Import external files and functions
-import user_input
+# import user_input
 
 
 def single_run_launch_platform_6dof(weather_model):
@@ -69,9 +69,49 @@ def single_run_orbit_propagation():
     return outputs
 
 
-def dispersion_analysis():
-    
+def dispersion_analysis(
+    inputs,
+    perturb_size,
+    num_sims,
+    distribution_type
+):
+    """
+    Generates distributed random parameters and runs given function
+
+    Inputs:
+    - 'inputs' = inputs to vary
+    - 'input_ranges' = ranges of inputs
+    - 'perturb_size' = perturbation size
+    - 'num_sims' = number of simulations to run
+    - 'distribution_type' = type of distribution (uniform, normal)
+
+    Outputs:
+    - 
+    """
     # TODO: develop logic to run a set of models in sequence, randomly varying all relevant parameters
+    # what do we need:
+    #   1. Balloon
+    #        - start pos (x,y,z)?, (alt, lat, lon)?
+    #        - 
+    #    2. Rocket
+    #        - start pos
+    #        - pointing angle
+    #        - 
+
+    ## Sample Generation
+    batch_size = 50
+    batches = np.floor(num_sims / batch_size)
+
+    for batch in range(1,batches+1):
+        perturbs = np.empty([np.size(inputs), batch_size])
+        if distribution_type == "uniform":
+            for (i,v) in enumerate(inputs):
+                perturbs[i] = np.random.uniform()
+        elif distribution_type == "normal":
+
+
+        perturbs = 
+
 
     outputs = 1
     return outputs
@@ -94,7 +134,7 @@ class EphemerisDataStruct:
     """
     current_time: t.datetime
     current_ref_frame: str
-    current_position_and_velocity: list[float]
+    # current_position_and_velocity: list[float]
     current_attitude: quat.Quaternion
 
 
@@ -105,11 +145,11 @@ if "__main__":
     All high-level framework logic and flow should be defined here
 
     """
-    inputs = user_input.user_input()
-    if inputs.mode == 1:
-        run_status, drift_altitude = single_run_launch_platform_6dof(inputs.weather_model)
-        if run_status == -1:
-            print('Run failed!')
-        elif run_status == 1:
-            print('\nRun succeeded!')
-            print("\nBalloon drifted to an altitude of {} km".format(drift_altitude))
+    # inputs = user_input.user_input()
+    # if inputs.mode == 1:
+    #     run_status, drift_altitude = single_run_launch_platform_6dof(inputs.weather_model)
+    #     if run_status == -1:
+    #         print('Run failed!')
+    #     elif run_status == 1:
+    #         print('\nRun succeeded!')
+    #         print("\nBalloon drifted to an altitude of {} km".format(drift_altitude))
